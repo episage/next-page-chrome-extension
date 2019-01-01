fecd8be59b14b1a22314014d40e1d892ad31c0bf
Tomasz Ciborski <tilosag@gmail.com>
2019-01-01 22:23:44 +0100

    _TRASH

--- src/backgroundScript.js
+++ src/backgroundScript.js
@@ -1,3 +1,48 @@
 chrome.runtime.onInstalled.addListener(function () {
-    
+
 });
+
+var database = [
+    {
+        "Stylish": "/* Displays images when using search preview extension. Example:https://www.google.com/search?q=food&num=20&source=lnms&tbm=nws&sa=X&ved=0ahUKEwjalvHv0OHWAhXD2SYKHSRwBqUQ_AUIDCgD */ @-moz-document domain(\"google.com\") { div.bkWMgd > div > div > a > img{ display: inline-block !important; }}",
+        "exampleUrl": "http://www.google.com/search?q=AutoPagerize\r\nhttp://www.google.co.jp/custom?q=firefox\r\nhttps://www.google.com/search?q=bucket&tbm=pts",
+        "comment": "Google Custom Search Result: http://wedata.net/items/60908\r\nGoogle Movies: http://wedata.net/items/58112\r\nGoogle Shopping: http://wedata.net/items/38952\r\nGoogle Custom Search Result: http://wedata.net/items/60908",
+        "url": "^https?://[^./]+\\.google(?:\\.[^./]{2,3}){1,2}/(?:c(?:se|ustom)|search|webhp|m|#|)",
+        "pageElement": "id('res')//li[div]|//div[@class='gsc-webResult gsc-result' or @class='psli']|id('rso')//div[contains(concat(\" \", normalize-space(@class), \" \"), \" g \") or contains(concat(\" \", normalize-space(@class), \" \"), \" g _cy \")]",
+        "nextLink": "id('pnnext')|id('navbar navcnt nav')//td[span]/following-sibling::td[1]/a|id('nn')/parent::a"
+    },
+    {
+        url: 'http[s]?://(.*).google.+/(search).+', // match url (RegExp)
+        nextLink: 'id("navbar")//td[last()]/a', // next link location (XPath)
+        pageElement: '//div[@id="res"]/div', // pagination insert location (XPath)
+        exampleUrl: 'http://www.google.com/search?q=nsIObserver', // sample URL for testing
+    },
+    {
+        urlRegExp: "",
+        pageItemXPath: "",
+        nextLinkXPath: "",
+    }
+];
+
+window.onload = function () {
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        if (request) {
+            var matchingDescriptor = database.find(d => {
+                return request.match(d.url);
+            });
+            // if (matchingDescriptor) {
+            //     log(`found a match`, matchingDescriptor);
+            // } else {
+            //     log(`this page has no matches`);
+            // }
+            if (!matchingDescriptor) {
+                return;
+            }
+
+            sendResponse(matchingDescriptor);
+        } else {
+            chrome.runtime.Port.disconnect();
+        }
+    });
+}
+
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,49 +1,18 @@
 window.addEventListener('load', () => {
-    var database = [
-        {
-            "Stylish": "/* Displays images when using search preview extension. Example:https://www.google.com/search?q=food&num=20&source=lnms&tbm=nws&sa=X&ved=0ahUKEwjalvHv0OHWAhXD2SYKHSRwBqUQ_AUIDCgD */ @-moz-document domain(\"google.com\") { div.bkWMgd > div > div > a > img{ display: inline-block !important; }}",
-            "exampleUrl": "http://www.google.com/search?q=AutoPagerize\r\nhttp://www.google.co.jp/custom?q=firefox\r\nhttps://www.google.com/search?q=bucket&tbm=pts",
-            "comment": "Google Custom Search Result: http://wedata.net/items/60908\r\nGoogle Movies: http://wedata.net/items/58112\r\nGoogle Shopping: http://wedata.net/items/38952\r\nGoogle Custom Search Result: http://wedata.net/items/60908",
-            "url": "^https?://[^./]+\\.google(?:\\.[^./]{2,3}){1,2}/(?:c(?:se|ustom)|search|webhp|m|#|)",
-            "pageElement": "id('res')//li[div]|//div[@class='gsc-webResult gsc-result' or @class='psli']|id('rso')//div[contains(concat(\" \", normalize-space(@class), \" \"), \" g \") or contains(concat(\" \", normalize-space(@class), \" \"), \" g _cy \")]",
-            "nextLink": "id('pnnext')|id('navbar navcnt nav')//td[span]/following-sibling::td[1]/a|id('nn')/parent::a"
-        },
-        {
-            url: 'http[s]?://(.*).google.+/(search).+', // match url (RegExp)
-            nextLink: 'id("navbar")//td[last()]/a', // next link location (XPath)
-            pageElement: '//div[@id="res"]/div', // pagination insert location (XPath)
-            exampleUrl: 'http://www.google.com/search?q=nsIObserver', // sample URL for testing
-        },
-        {
-            urlRegExp: "",
-            pageItemXPath: "",
-            nextLinkXPath: "",
+    chrome.runtime.sendMessage(window.location, function (response) {
+        if(!response){
+            return;
         }
-    ];
 
-    var options = {
-
-    };
-
-    run(options, database);
+        run(options, response);
+    });
 });
 
 var log = console.debug;
 
-async function run(options, database) {
-    // step 1, match url
-    var matchingDescriptor = database.find(d => {
-        return window.location.href.match(d.url);
-    });
-    if (matchingDescriptor) {
-        log(`found a match`, matchingDescriptor);
-    } else {
-        log(`this page has no matches`);
-    }
-    if (!matchingDescriptor) {
-        return;
-    }
-
+async function run(options, matchingDescriptor) {
+    console.log(matchingDescriptor);
+    
     loop(window.document, null, 2, loop);
 
     return;
